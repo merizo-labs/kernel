a56dc5e338bd280a720e4fa8ab67a98d1b21180f
oblivisheee <molniya213y@gmail.com>
2025-01-31 21:35:53 +0300

    added talc

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@
 mod vga_buffer;
 
 use panic_halt as _;
+use spin::Mutex;
+use talc::{ClaimOnOom, Span, Talc};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    Span::empty();
     loop {}
 }
